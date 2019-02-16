512ae2ceb65e5769bc6fdbd6be816010af43af2d
Unknown <kirby94@hotmail.it>
2019-02-16 16:21:08 +0100

    Add explorationRate

--- src/main/java/QTable.java
+++ src/main/java/QTable.java
@@ -4,9 +4,13 @@ public class QTable {
 
     double[][] qualities;
 
+    double explorationRate;
+
     public QTable() {
         int states = Tile.values().length;
         int actions = Action.values().length;
         qualities = new double[actions][states];
+
+        explorationRate = 1;
     }
 }
